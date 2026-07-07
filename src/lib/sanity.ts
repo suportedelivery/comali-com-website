5aa4b1d435270398b5c10fed50f85d75f498929f
Comali <atendimento@comali.com.br>
2026-07-07 03:31:13 -0300

    fix: disable Sanity CDN cache for real-time data

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -5,7 +5,7 @@ export const sanityConfig = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false,
 }
 
 export const sanityClient = createClient(sanityConfig)
